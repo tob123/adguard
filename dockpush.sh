ae121ec122762866dfcd3428c08be76e58253b39
tob <tob@nice.eu>
2020-10-24 09:46:51 +0200

    add more tags

--- dockpush.sh
+++ dockpush.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 set -ex
 docker push tob123/adguard-staging:${VERSION}
+if [[ -n $LATEST_MINOR ]]; then
+  MAJOR_TAG=$(echo $VERSION | awk -F. {' print $1"."$2'})
+  docker tag tob123/adguard-staging:${VERSION} tob123/adguard-staging:${MAJOR_TAG}
+  docker push tob123/adguard-staging:${MAJOR_TAG}
+fi
 if [[ -n $LATEST ]]; then
   docker tag tob123/adguard-staging:${VERSION} tob123/adguard-staging:latest
   docker push tob123/adguard-staging:latest
