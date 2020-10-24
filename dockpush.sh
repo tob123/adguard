00f141d38830bb0f05a5be90234e49938b18cffb
tob <tob@nice.eu>
2020-10-24 09:23:34 +0200

    various fixes for travis

--- dockpush.sh
+++ dockpush.sh
@@ -3,5 +3,5 @@ set -ex
 docker push tob123/adguard-staging:${VERSION}
 if [[ -n $LATEST ]]; then
   docker tag tob123/adguard-staging:${VERSION} tob123/adguard-staging:latest
-  docker push tob123/piwigo-staging:latest
+  docker push tob123/adguard-staging:latest
 fi
